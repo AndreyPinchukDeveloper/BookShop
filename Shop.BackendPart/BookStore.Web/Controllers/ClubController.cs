7fa8322bd42be655e4ffdb5581e91bfc2971aadd
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2023-06-10 22:05:16 +0700

    dev Update
    - controllers and views for race/club.

---
+++ Shop.BackendPart/BookStore.Web/Controllers/ClubController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Web.Controllers
+{
+    public class ClubController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Shop.BackendPart/BookStore.Web/Controllers/RaceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Web.Controllers
+{
+    public class RaceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
