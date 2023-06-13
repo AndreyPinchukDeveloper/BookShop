3fcfa7802a19e0ac1be83b30c5c7c3802a619fd6
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2023-06-13 23:05:59 +0700

    dev Update
    - view part for all controllers;
    - connected all views to the main _layout.

--- Shop.BackendPart/BookStore.Web/Controllers/RaceController.cs
+++ Shop.BackendPart/BookStore.Web/Controllers/RaceController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookStore.Web.Data;
+using BookStore.Web.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookStore.Web.Controllers
 {
     public class RaceController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public RaceController(AppDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Race> races = _context.Races.ToList();
+            return View(races);
         }
     }
 }
