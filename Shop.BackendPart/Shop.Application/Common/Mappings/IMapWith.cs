cadd2b014f1eed6f4456252f5d3551b6a6a8a588
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2023-09-28 21:30:59 +0700

    dev update
    - added and tune(make all settings) for AutoMapper.

---
+++ Shop.BackendPart/Shop.Application/Common/Mappings/IMapWith.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace Shop.Application.Common.Mappings
+{
+    public interface IMapWith<T>
+    {
+        void Mapping(Profile profile) =>
+            profile.CreateMap(typeof(T), GetType());
+    }
+}
