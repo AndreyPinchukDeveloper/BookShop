d2dee6e750fe0b6d7e05606e954cd39573b1b969
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2023-10-02 19:13:44 +0700

    dev Update
    - added DependecyInjection classes.

---
+++ Shop.BackendPart/Shop.Application/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Shop.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
---
+++ Shop.BackendPart/Shop.Persistence/DependencyImjection.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Shop.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Persistence
+{
+    /// <summary>
+    /// Add db context to use and register it
+    /// </summary>
+    public static class DependencyImjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration["DbConnection"];
+            services.AddDbContext<BooksDbContext>(options =>
+            {
+                options.UseSqlite(connectionString);
+            });
+            services.AddScoped<IBooksDbContext>(provider =>
+                provider.GetService<BooksDbContext>());
+            return services;
+        }
+    }
+}
